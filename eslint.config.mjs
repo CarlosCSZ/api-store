ad35c14ce159007aee356d445eb17807dc284397
CarlosCSZ <carlos.cherrez@globant.com>
2025-03-25 00:09:40 -0500

    FEAT: established connection to mongodb. ADD: eslint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  eslintPluginPrettierRecommended,
-  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: eslintPluginPrettierRecommended,
+    },
     languageOptions: {
+      parser: tseslint.parser,
       globals: {
         ...globals.node,
         ...globals.jest,
@@ -23,12 +25,14 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
+    ignores: ['eslint.config.mjs'],
     rules: {
+      'prettier/prettier': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-var-requires': 'error',
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from '@common/filters/http-exception.filter';
+import { AppConfig } from '@shared/config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableShutdownHooks();
+
+  const config = app.get<ConfigService<AppConfig>>(ConfigService);
+  try {
+    await app.listen(config.get<number>('httPort') ?? 3000);
+    console.log(`API Store is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error({ err: error }, 'Application start error');
+    process.exit(1);
+  }
 }
 bootstrap();
